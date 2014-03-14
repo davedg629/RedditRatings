d22147a3327f13907bd008f91890c2e01258e604
David DiGiovanni <davedg629@gmail.com>
2014-03-13 21:16:33 -0400

    Add manage.py for Flask-Script commands

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
-import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.script import Manager
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+manager = Manager(app)
 
 from app import models, views, admin_views
 from admin_views import admin
